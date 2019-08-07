5bd68abe7085bc156ee8002439dd93d5ebc05585
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2019-08-07 09:15:37 -0500

    Primer Commit

---
+++ Assets/Avatar.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar : MonoBehaviour
+{
+    public string Nombre;
+    public Color MiColor;
+    public int Edad;
+    public float Peso, speed;
+
+    Transform objective;
+    bool arrived=true, directed=false;
+
+    Renderer mat;
+    Rigidbody rig;
+
+    Vector3 direction;
+
+    void Start()
+    {
+        MiColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        mat = transform.GetChild(1).GetComponent<Renderer>();
+        speed = Random.Range(1f, 5f);
+        mat.material.SetColor("_Color",MiColor);
+        rig = GetComponent<Rigidbody>();
+
+        Edad = Random.Range(0, 100);
+        Peso = Random.Range(50f, 100f);
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!arrived)
+        {
+            rig.MovePosition(transform.position + direction * Time.deltaTime * speed);
+
+            if (!directed) {
+                directed = true;
+                StartCoroutine(setDirection(0.3f));
+            }
+
+            float dist = Vector3.Distance(transform.position, objective.position);
+
+            if (dist < 1.5f)
+            {
+                arrived = true;
+            }
+        }
+    }
+
+    IEnumerator setDirection(float time)
+    {
+        direction = objective.position - transform.position;
+        direction.y = 0;
+        direction.Normalize();
+        yield return new WaitForSeconds(time);
+        directed = false;
+    }
+
+    public void setObjective(Transform obj, bool arrive)
+    {
+        objective = obj;
+        arrived = arrive;
+
+    }
+
+    public void GotoObjective()
+    {
+        arrived = false;
+        direction = objective.position - transform.position;
+        direction.y = 0;
+        direction.Normalize();
+    }
+}
---
+++ Assets/Manager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+
+    List<Avatar> Todos;
+    List<Transform> objetivos;
+    public GameObject miiAvatar;
+    [Tooltip("Cuantos mii´s, habrá en la simulacion")]
+    public int NumeroAvatares;
+
+    public float limitex=10, limitez=10;
+
+   
+    void Start()
+    {
+        Todos = new List<Avatar>();
+        objetivos = new List<Transform>();
+
+        for (int i=0; i < NumeroAvatares; i++) {
+            GameObject go=Instantiate(miiAvatar, new Vector3(Random.Range(-5f,5f), 0, Random.Range(-5, 5)), Quaternion.identity);
+            Avatar currentAvatar = go.GetComponent<Avatar>();
+            Todos.Add(currentAvatar);
+
+            GameObject obj = new GameObject();
+            /*Instantiate(obj, transform.position, Quaternion.identity);*/
+            Transform currentrans = obj.GetComponent<Transform>();
+            //print(currentrans);
+            objetivos.Add(currentrans);
+            currentAvatar.setObjective(currentrans, true);
+
+        }
+
+        setWaypoints();
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void OrdenarPorEdad()
+    {
+        AvatarSortByEdad AvSortEdad = new AvatarSortByEdad();
+        Todos.Sort(AvSortEdad);
+        foreach (Avatar ar in Todos)
+        {
+            ar.GotoObjective();
+        }
+    }
+
+    public void OrdenarPorPeso()
+    {
+        AvatarSortByWeight AvSortPeso = new AvatarSortByWeight();
+        Todos.Sort(AvSortPeso);
+
+        foreach(Avatar ar in Todos)
+        {
+            ar.GotoObjective();
+        }
+    }
+
+    void setWaypoints()
+    {
+        float espacioX = limitex*2;  //20
+        float espacioZ = limitez*2; //20
+
+        int columnas = NumeroAvatares/10; //10
+
+        float distanciaX = espacioX / columnas;
+        float distanciaZ = espacioZ / columnas;
+
+        float contX = -limitex, contZ = -limitez;
+       
+        foreach(Transform tr in objetivos)
+        {
+            
+            tr.position = new Vector3(contX, 1, contZ);
+            contX += distanciaX; 
+            if (contX > espacioX+0.001)
+            {
+                contX = -limitex;
+                contZ += distanciaZ;
+            }
+
+        }
+    }
+}
+
+class AvatarSortByEdad : IComparer<Avatar>
+{
+    #region IComparer<Avatar> Members
+
+    public int Compare(Avatar x, Avatar y)
+    {
+        if (x.Edad > y.Edad) return 1;
+        else if (x.Edad < y.Edad) return -1;
+        else return 0;
+    }
+
+    #endregion
+}
+
+class AvatarSortByWeight : IComparer<Avatar>
+{
+    #region IComparer<Avatar> Members
+
+    public int Compare(Avatar x, Avatar y)
+    {
+        if (x.Peso > y.Peso) return 1;
+        else if (x.Peso < y.Peso) return -1;
+        else return 0;
+    }
+
+    #endregion
+}
